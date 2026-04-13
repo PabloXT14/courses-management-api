19bf6d5dead7848927954b7ed0f469bdc08d1c77
Pablo Alan <pabloxt14@gmail.com>
2026-04-13 17:50:18 -0300

    feat: made an join between tables

--- src/routes/get-courses.ts
+++ src/routes/get-courses.ts
@@ -1,9 +1,9 @@
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import z from "zod";
-import { ilike, asc, SQL, and } from "drizzle-orm";
+import { ilike, asc, SQL, and, eq, count } from "drizzle-orm";
 
 import { db } from "../database/client.ts";
-import { courses } from "../database/schema.ts";
+import { courses, enrollments } from "../database/schema.ts";
 
 export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
   server.get("/courses", {
@@ -19,7 +19,8 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
         200: z.object({
           courses: z.array(z.object({
             id: z.string(),
-            title: z.string()
+            title: z.string(),
+            enrollments: z.number()
           })),
           total: z.number()
         })
@@ -37,12 +38,15 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
     const [result, totalCount] = await Promise.all([
       db.select({
         id: courses.id,
-        title: courses.title
+        title: courses.title,
+        enrollments: count(enrollments.id)
       })
         .from(courses)
+        .leftJoin(enrollments, eq(enrollments.courseId, courses.id))
         .where(
           and(...conditions)
         )
+        .groupBy(courses.id)
         .orderBy(asc(courses[orderBy]))
         .limit(10)
         .offset((page - 1) * 10),
